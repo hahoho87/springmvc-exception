57ad9d67ae4cf41e2e32ddb0dc6092df4b2f8e53
hahoho87 <hahoho87@gmail.com>
2021-09-01 08:07:21 +0900

    서블릿 예외 처리 - interceptor

--- src/main/java/com/hahoho87/exception/servlet/WebConfig.java
+++ src/main/java/com/hahoho87/exception/servlet/WebConfig.java
@@ -1,9 +1,10 @@
 package com.hahoho87.exception.servlet;
 
 import com.hahoho87.exception.filter.LogFilter;
+import com.hahoho87.exception.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.DispatcherType;
@@ -12,7 +13,15 @@ import javax.servlet.Filter;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Bean
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/*", "*.ico", "/error", "/error-page/**");
+    }
+
+//    @Bean
     public FilterRegistrationBean<Filter> logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
