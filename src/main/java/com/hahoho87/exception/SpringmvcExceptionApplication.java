ede5e4926f4dea60e2cdb7216aebbb67343da31c
bky4564 <bky4564@hunet.co.kr>
2021-08-31 12:29:20 +0900

    Initial commit

---
+++ src/main/java/com/hahoho87/exception/SpringmvcExceptionApplication.java
@@ -0,0 +1,13 @@
+package com.hahoho87.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmvcExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringmvcExceptionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hahoho87/exception/SpringmvcExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hahoho87.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
